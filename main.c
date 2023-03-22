78ca08746fe36e6d8a7968a3faac1012f8e30363
Egidius Mengelberg <mengelberg@mengelberg.eu>
2023-03-22 14:00:48 +0100

    v1.0.0

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 
 //global variables
-bool linkLed = false;
+bool blinkLed = false;
 
 // config
 
@@ -33,6 +33,17 @@ bool linkLed = false;
 #define BUTTON_DOWN_PIN PINB
 #define BUTTON_DOWN_NUM 2
 
+//endstop_min
+#define ENDSTOP_MIN_DDR DDRB
+#define ENDSTOP_MIN_PORT PORTB
+#define ENDSTOP_MIN_PIN PINB
+#define ENDSTOP_MIN_NUM 2
+//endstop_max
+#define ENDSTOP_MAX_DDR DDRB
+#define ENDSTOP_MAX_PORT PORTB
+#define ENDSTOP_MAX_PIN PINB
+#define ENDSTOP_MAX_NUM 2
+
 //functions
 void initMotor();
 void moveMotor(bool direction);
@@ -48,7 +59,24 @@ int main(void) {
 	initButtons();
 	initLed();
 
-    while(1) {}
+    while(1) {
+		// while up pressed and not up endstop move: up and blink
+		while (upPressed && !maxPressed)
+		{
+			ledOn();
+			moveMotor(true);
+		}
+		
+		// while down pressed and not down endstop: move down and blink
+		while (downPressed && !minPressed)
+		{
+			ledOn();
+			moveMotor(false);
+		}
+
+		ledOff();
+		stopMotor();
+	}
 }
 
 void initMotor() {
@@ -91,6 +119,30 @@ void initButtons() {
     //button down
     BUTTON_DOWN_DDR &= ~(1 << BUTTON_DOWN_NUM);
     BUTTON_DOWN_PORT |= (1 << BUTTON_DOWN_NUM);
+
+	//endstop min
+    ENDSTOP_MIN_DDR &= ~(1 << ENDSTOP_MIN_NUM);
+    ENDSTOP_MIN_PORT |= (1 << ENDSTOP_MIN_NUM);
+
+    //endstop max
+    ENDSTOP_MAX_DDR &= ~(1 << ENDSTOP_MAX_NUM);
+    ENDSTOP_MAX_PORT |= (1 << ENDSTOP_MAX_NUM);
+}
+
+bool upPressed() {
+    return !(BUTTON_UP_PIN & (1 << BUTTON_UP_NUM));
+}
+
+bool downPressed() {
+    return !(BUTTON_DOWN_PIN & (1 << BUTTON_DOWN_NUM));
+}
+
+bool minPressed() {
+    return !(ENDSTOP_MIN_PIN & (1 << ENDSTOP_MIN_NUM));
+}
+
+bool maxPressed() {
+    return !(ENDSTOP_MAX_PIN & (1 << ENDSTOP_MAX_NUM));
 }
 
 void initLed(void) {
@@ -112,16 +164,16 @@ void initLed(void) {
 }
 
 void ledOn(void) {
-    linkLed = true;
+    blinkLed = true;
 }
 
 void ledOff(void) {
-    linkLed = false;
+    blinkLed = false;
     LED_PORT &= ~(1 << LED_NUM);
 }
 
 ISR(TIMER4_COMPA_vect){
-    if (linkLed) {
+    if (blinkLed) {
         LED_PORT ^= (1 << LED_NUM);
     }
 }
